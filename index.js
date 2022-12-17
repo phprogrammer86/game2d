5e905b80a19697bcce3089dba365d71f5d2b9308
phprogrammer86 <105813621+phprogrammer86@users.noreply.github.com>
2022-12-17 19:05:57 -0300

    adicionando as classes em arquivos diferentes e criando um um grafico para os suportes do floor

--- index.js
+++ index.js
@@ -12,52 +12,28 @@ const scaledCanvas = {
     height: canvas.height / 4
 }
 
+const floorCollisions2D = []
+for (let i = 0; i < floorCollisions.length; i += 36) {
+    floorCollisions2D.push(floorCollisions.slice(i, i + 36))
+}
+
+const collisionBlocks = []
+floorCollisions2D.forEach((row, y) => {
+    row.forEach((Symbol, x) => {
+        if (Symbol === 202) {
+            collisionBlocks.push(
+                new CollisionBlocks({
+                    position: {
+                        x: x * 16,
+                        y: y * 16,
+                    }
+                })
+            )
+        }
+    })
+})
+
 const gravity = 0.5;
-
-class Sprite {
-    constructor({position, imageSrc}) {
-        this.position = position
-        this.image = new Image()
-        this.image.src = imageSrc
-    }
-
-    draw() {
-        if(!this.image) return 
-        c.drawImage(this.image, this.position.x, this.position.y)
-    }
-
-    update() {
-        this.draw()
-    }
-}
-
-class Player {
-    constructor(position)  {
-        this.position = position
-        this.velocity = {
-            x: 0,
-            y: 1,
-        }
-        this.height = 100
-        }
-    
-
-
-draw() {
-    c.fillStyle = 'red';
-    //dimensionando o retangulo
-    c.fillRect(this.position.x, this.position.y, 100, this.height)
-}
-
-update () {
-    this.draw();
-    this.position.y += this.velocity.y
-    this.position.x += this.velocity.x
-    if (this.position.y + this.height + this.velocity.y < canvas.height)
-    this.velocity.y += gravity
-    else this.velocity.y = 0
-  }
-}
 
 const player = new Player({
     x: 0,
@@ -91,10 +67,13 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height); 
 
     c.save()
-    c.scale(4, 4)
-    c.translate(0, -background.image.height + scaledCanvas.height)
+      
     background.update();
+    collisionBlocks.forEach((collisionBlock) => {
+        collisionBlock.update()
+    })
     c.restore()
+
 
     player.update();
     player2.update();
